ac2925827c13621e0a237aea2fd8053375c826fc
Nesser Andrade <nesser24601@live.com>
2021-11-12 20:00:55 -0300

    quarto commit

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -5,10 +5,12 @@ import "../../App.css"
 const ProductItem = ({ name, cost, img, description }) => {
   return (
     <div className="product-item">
-        <img className="img-product" src={img} alt={name}></img>
+        <img className="img-product" src={img} alt={name} />
+        <div className="product-body">
         <p className="product-name">{name}</p>
         <p className="product-cost">R${cost}</p>
         <p className="product-description">Descrição: {description}</p>
+        </div>
     </div>
   );
 };
