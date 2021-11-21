9213266e4b2031ba60a66aa9c60cca55fd787358
Nesser Andrade <nesser24601@live.com>
2021-11-21 19:24:20 -0300

    mudando cores

--- src/components/MenuHome/index.jsx
+++ src/components/MenuHome/index.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const MenuHome = ({title}) => {
     return (
       <header className="header-container">
         <h1 className="header-title">{title}</h1>
-        <Link to="/login">
-        <button className="botao-menu">Login</button>
-        </Link>
       </header>
     );
 }
